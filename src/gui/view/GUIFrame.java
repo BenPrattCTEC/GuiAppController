168d44671586ae8dbaff229feb45c93d0c548c64
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-11-13 13:18:37 -0700

    made color change button

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -5,19 +5,24 @@ import javax.swing.JFrame;
 
 public class GUIFrame extends JFrame{
 	private GUIAppController controller;
+	private GUIPanel panel;
 	
 	public GUIFrame(GUIAppController controller){
 		super();
+		
 		this.controller = controller;
+		this.panel = new GUIPanel(controller);
+		
 		setupFrame();
 		
 	}
 	
 	private void setupFrame(){
 		
-		
+		this.setContentPane(panel);
 		this.setTitle("GUI Testing");
 		this.setSize(500, 500);
+		
 		this.setVisible(true);
 	}
 	
--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -1,16 +1,63 @@
 package gui.view;
 
 import gui.controller.GUIAppController;
-import java.awt.Color;
+import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.*;
 
-public class GUIPanel extends JPanel{
-
+public class GUIPanel extends JPanel {
+	
 	private GUIAppController controller;
 	
-	public GUIPanel(GUIAppController controller){
+	private JButton quitButton;
+	private JButton colorChangeButton;
+	private SpringLayout layout;
+	
+	public GUIPanel(GUIAppController controller) {
 		super();
 		this.controller = controller;
+		
+		layout = new SpringLayout();
+		
+		quitButton = new JButton("Quit");
+		layout.putConstraint(SpringLayout.WEST, quitButton, 193, SpringLayout.WEST, this);
+		colorChangeButton = new JButton("Change Color");
+		layout.putConstraint(SpringLayout.WEST, colorChangeButton, 160, SpringLayout.WEST, this);
+		layout.putConstraint(SpringLayout.SOUTH, colorChangeButton, -105, SpringLayout.SOUTH, this);
+		layout.putConstraint(SpringLayout.NORTH, quitButton, 33, SpringLayout.SOUTH, colorChangeButton);
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel() {
+		this.setBackground(Color.darkGray);
+		this.setLayout(layout);
+		this.add(quitButton);
+		this.add(colorChangeButton);
+	}
+	
+	private void setupLayout() {
+		System.out.println(super.getWidth());
+	}
+	
+	private void setupListeners() {
+		quitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent click) {
+				System.exit(0);
+			}
+		});
+		colorChangeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent click) {
+				randomBackgroundColor();
+			}
+		});
+	}
+	
+	private void randomBackgroundColor(){
+		this.setBackground(new Color((int)(Math.random()*256), (int)(Math.random()*256), (int)(Math.random()*256)));
 	}
 	
 }

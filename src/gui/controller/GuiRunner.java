9015dbd6d915b44b5a118e8b0fb480663e13a7fb
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-11-13 11:40:23 -0700

    added runner and controller

---
+++ src/gui/controller/GuiRunner.java
@@ -0,0 +1,13 @@
+package gui.controller;
+
+import gui.controller.GuiController;
+
+public class GuiRunner {
+	
+	public static void main(String[] args) {
+		GuiController runner = new GuiController();
+		runner.start();
+		
+	}
+	
+}
